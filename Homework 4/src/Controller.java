fdb0e0144d4e8a02c5c9998ea70a48b3bc2c2215
Gunny576 <garrettorme@gmail.com>
2015-04-02 08:51:21 -0500

    added initial Code
    added initial Code to github

---
+++ Homework 4/src/Controller.java
@@ -0,0 +1,31 @@
+
+public class Controller {
+	ShoppingCart SCart = new ShoppingCart();
+	public Controller()
+	{
+		
+	}
+	public void run(String[][] database)
+	{
+		//get input
+		
+		int input =0;
+		
+		switch (input)
+		{
+		case 1:
+		//add call	
+		break;
+		
+		case 2:
+		//remove call
+		break;
+		
+		case 3:
+		//checkout call
+		break;
+		}
+		
+		
+	}
+}
