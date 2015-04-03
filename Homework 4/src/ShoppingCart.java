7b98608ac3e1d0c05ad386ba8c7006980c6cece8
unknown <gorme@1005C-10.eecs.ku.edu>
2015-04-03 10:36:06 -0500

    Upload of new Shopping Cart

--- Homework 4/src/ShoppingCart.java
+++ Homework 4/src/ShoppingCart.java
@@ -1,25 +1,41 @@
 import java.util.*;
 public class ShoppingCart {
 public int total;
-Queue cart = new LinkedList();
+List cart = new ArrayList();
+int index;
+int index_item;
 	public ShoppingCart()
 	{
 		total = 0;
+		index = 0;
 	}
 	
 	public void add(String item, int price)
 	{
 		total = total + price;
-		cart.add(item);
+		cart.add(index,item);
+		index++;
 	}
 	
 	public void remove(String item, int price)
 	{
 		total = total - price;
-		cart.remove();
+		index_item = cart.indexOf(item);
+		cart.remove(index_item);
+		index --;
 	}
+
 	public int getTotal()
 	{
 		return total;
 	}
+	public String[] getList(){
+		
+		String[] array = new String[index];
+		for(int i = 0; i < index; i++){
+			array[i] = (String) cart.get(i);
+		}
+		return array;
+	}	
+
 }
